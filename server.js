d703f7492b8d050bbc08f35338d2fb6fa90df74e
ashwaninsit <ashwanisinghnet@gmail.com>
2018-03-10 14:51:42 +0530

    Update server.js
    updated regex

--- server.js
+++ server.js
@@ -7,44 +7,28 @@ const CircularJSON = require('circular-json');
 const port = process.env.PORT || 5000;
 var cors = require('cors');
 app.use(cors());
-/*
-app.get('/api/:id', (req, res,next,val) => {
-  res.send({ express: 'Hello From Express'+val });
-  next();
-});
-*/
 
 const mp=new Map();
+
 
 request.get('http://terriblytinytales.com/test.txt', function (error, response, body) {
     if (!error && response.statusCode == 200) {
         var text = body;
         var temp;
-
         var strtemp="";
         var str=[];
-        // Continue with your processing here.
-        for(var i=0;i<text.length;i++)
-         {
-          temp=text[i].charCodeAt(0);
-          if(text[i]==' '||text[i]=='\n')
-           {
-            if(strtemp!="")
-            {
-              strtemp=strtemp.toLowerCase();
-            str.push(strtemp);
 
-            }
-            strtemp="";
-           }
-           else {
-             if((temp>=65&&temp<=90)||(temp>=97&&temp<=122)||temp==39)
-             {
-              strtemp+=text[i];
-             }
-           }
-         }
-      str=sw.removeStopwords(str);
+         str=text.split(/[\s,]+/);
+         //console.log(str);
+
+      for(var i=0;i<str.length;i++)
+       {
+        str[i]=str[i].toLowerCase();
+       }
+
+
+        //  Comment the next line if you want to include stopwords also
+       str=sw.removeStopwords(str);
       for(var i=0;i<str.length;i++)
        {
          mp.set(str[i],0);
@@ -84,7 +68,7 @@ request.get('http://terriblytinytales.com/test.txt', function (error, response, 
               var temp=[];
               if(id=="")
                {
-               res.send(JSON.stringify(temp));     
+               res.send(JSON.stringify(temp));
                }
                else {
               for(var i=0;i<Math.min(id,count.length);i++)
